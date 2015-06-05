3e3e692e699bd0bd46ac3476eb7b5c001292d68b
Ben Finke <ben@benfinke.com>
2015-06-05 11:25:19 -0400

    configured nmap_targets models.py and migrated DB

--- hubble/nmap_targets/models.py
+++ hubble/nmap_targets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Target_Group(models.Model):
+    group_name = models.CharField(max_length=50)
+    group_desc = models.CharField(max_length=200)
+    create_date = models.DateTimeField('date created')
+
+class Target_Tags(models.Model):
+    tag_name = models.CharField(max_length=50)
+    tag_desc = models.CharField(max_length=200)
+    create_date = models.DateTimeField('date created')
+
+class Target(models.Model):
+    target_entry = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
